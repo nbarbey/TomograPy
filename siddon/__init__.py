b29c95d28b2967382cb0e3a19f8177652579e3db
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-23 15:57:27 +0200

    Add doc to package

--- siddon/__init__.py
+++ siddon/__init__.py
@@ -1,6 +1,20 @@
 """
-This modules implement the python version of Siddon algorithm
-and solar tomography reconstructions
+This packages implements tomography algorithms and utilities in
+python. It is made of several modules
+
+- siddon: is the core of the package, with a fast C/OpenMP
+  implementation of the Siddon algorithm :
+  See : http://adsabs.harvard.edu/abs/1985MedPh..12..252S
+
+- simu: implements some utilities to perform simulations
+
+- secchi: a module to load STEREO/SECCHI data with appropriate
+  metadata
+
+- phantom: To generate phantoms (Shepp-Logan, Modified Shepp Logan,
+  and Yu Ye Wang phantoms).
+
+
 """
 
 from siddon import *
