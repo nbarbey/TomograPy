ae6e3408c189dc836442208684fe6e0f070563c3
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-24 15:37:35 +0200

    a test with lo and one without

--- exemples/test_siddon_simu_lo.py
+++ exemples/test_siddon_simu_lo.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import scipy.sparse.linalg as spl
 import siddon
+import lo
 # object
 header = {'SIMPLE':True,'BITPIX':-64,
           'NAXIS1':32, 'NAXIS2':32, 'NAXIS3':32,
@@ -23,11 +24,31 @@ image_header = {'n_images':60,
 image_header['radius'] = 200.
 data = siddon.simu.circular_trajectory_data(**image_header)
 data[:] = np.zeros(data.shape)
+# projector
+P = siddon.siddon_lo(data.header, obj.header)
 # projection
 t = time.time()
 data = siddon.projector(data, obj)
 print("projection time : " + str(time.time() - t))
+# data
+y = data.flatten()
 # backprojection
 t = time.time()
-x0 = siddon.backprojector(data, obj.copy())
-print("backprojection time : " + str(time.time() - t))
+x0 = P.T * y
+bpj = x0.reshape(obj.shape)
+print("projection time : " + str(time.time() - t))
+# coverage map
+weights = (P.T * np.ones(y.size)).reshape(obj.shape)
+# priors
+Ds = [lo.diff(obj.shape, axis=i) for i in xrange(3)]
+hypers = 1e0 * np.ones(3)
+#Ds, hypers = [], []
+# inversion using scipy.sparse.linalg
+t = time.time()
+tol = 1e-5
+sol, info = lo.rls(P, y, Ds, hypers,  maxiter=100, tol=tol)
+sol = sol.reshape(bpj.shape)
+if info != 0:
+    print("Inversion algorithm did not converge to " + str(tol))
+
+print("inversion time : " + str(time.time() - t))
