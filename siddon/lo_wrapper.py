7873a3356b07ceafe7bcec434c2c8f2fc11c993c
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-24 16:22:30 +0200

    lo is no longer mandatory to use siddon

---
+++ siddon/lo_wrapper.py
@@ -0,0 +1,49 @@
+"""
+If lo package is present, define siddon lo wrapper
+"""
+import lo
+
+def siddon_lo(data_header, cube_header):
+    """
+    A linear operator performing projection and backprojection using
+    Siddon.
+    """
+    data = dataarray_from_header(data_header)
+    data[:] = 0
+    cube = fa.fitsarray_from_header(cube_header)
+    cube[:] = 0
+    def matvec(x):
+        y = dataarray_from_header(data_header)
+        y[:] = 0
+        projector(y, x)
+        return y
+    def rmatvec(x):
+        y = fa.fitsarray_from_header(cube_header)
+        y[:] = 0
+        backprojector(x, y)
+        return y
+    return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec)
+
+def siddon_sun_lo(data_header, cube_header):
+    """
+    A linear operator performing projection and backprojection using
+    Siddon.
+    Rays are blocked at a centered sphere of radius one (as it is the
+    case in solar tomography).
+    """
+    data = dataarray_from_header(data_header)
+    data[:] = 0
+    cube = fa.fitsarray_from_header(cube_header)
+    cube[:] = 0
+    def matvec(x):
+        y = dataarray_from_header(data_header)
+        y[:] = 0
+        projector_sun(y, x)
+        return y
+    def rmatvec(x):
+        y = fa.fitsarray_from_header(cube_header)
+        y[:] = 0
+        backprojector_sun(x, y)
+        return y
+    return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec)
+
--- siddon/siddon.py
+++ siddon/siddon.py
@@ -60,7 +60,6 @@ import copy
 import os
 import pyfits
 import fitsarray as fa
-import lo
 from _C_siddon import siddon_sun as C_siddon_sun
 from _C_siddon import siddon as C_siddon
 
@@ -81,27 +80,6 @@ def backprojector(data, cube):
     C_siddon(data, cube, 1)
     return cube
 
-def siddon_lo(data_header, cube_header):
-    """
-    A linear operator performing projection and backprojection using
-    Siddon.
-    """
-    data = dataarray_from_header(data_header)
-    data[:] = 0
-    cube = fa.fitsarray_from_header(cube_header)
-    cube[:] = 0
-    def matvec(x):
-        y = dataarray_from_header(data_header)
-        y[:] = 0
-        projector(y, x)
-        return y
-    def rmatvec(x):
-        y = fa.fitsarray_from_header(cube_header)
-        y[:] = 0
-        backprojector(x, y)
-        return y
-    return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec)
-
 def projector_sun(data, cube):
     """
     Project a cubic map into a data cube using the Siddon algorithm.
@@ -121,29 +99,6 @@ def backprojector_sun(data, cube):
     cube.header = dict(cube.header)
     C_siddon_sun(data, cube, 1)
     return cube
-
-def siddon_sun_lo(data_header, cube_header):
-    """
-    A linear operator performing projection and backprojection using
-    Siddon.
-    Rays are blocked at a centered sphere of radius one (as it is the
-    case in solar tomography).
-    """
-    data = dataarray_from_header(data_header)
-    data[:] = 0
-    cube = fa.fitsarray_from_header(cube_header)
-    cube[:] = 0
-    def matvec(x):
-        y = dataarray_from_header(data_header)
-        y[:] = 0
-        projector_sun(y, x)
-        return y
-    def rmatvec(x):
-        y = fa.fitsarray_from_header(cube_header)
-        y[:] = 0
-        backprojector_sun(x, y)
-        return y
-    return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec)
 
 def dataarray_from_header(header):
     """
