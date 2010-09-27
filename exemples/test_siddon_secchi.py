eb43b857504b3afebf1e539a20a55f7ed0a2acec
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-27 17:39:07 +0200

    use the new obstacle keyword

--- exemples/test_siddon_secchi.py
+++ exemples/test_siddon_secchi.py
@@ -8,9 +8,9 @@ import fitsarray as fa
 # data 
 path = os.path.join(os.getenv('HOME'), 'data', '171dec08')
 obsrvtry = 'STEREO_A'
-time_window = ['2008-12-01T00:00:00.000', '2008-12-03T00:00:00.000']
-time_step = 4 * 3600. # one image every time_step seconds
-data = siddon.secchi.read_data(path, bin_factor=4,
+time_window = ['2008-12-01T00:00:00.000', '2008-12-15T00:00:00.000']
+time_step = 8 * 3600. # one image every time_step seconds
+data = siddon.secchi.read_data(path, bin_factor=8,
                                obsrvtry=obsrvtry,
                                time_window=time_window, 
                                time_step=time_step)
@@ -21,5 +21,5 @@ header = {'CRPIX1':64., 'CRPIX2':64., 'CRPIX3':64.,
           'CRVAL1':0., 'CRVAL2':0., 'CRVAL3':0.,}
 cube = fa.zeros(shape, header=header)
 t = time.time()
-siddon.backprojector_sun(data, cube)
+cube = siddon.backprojector(data, cube, obstacle="sun")
 print("backprojection time : " + str(time.time() - t))
