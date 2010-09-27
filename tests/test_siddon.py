1e9eb6df8860bf9cff681a24d62c4a69dd64dba7
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-27 19:36:41 +0200

    added very basis test

--- tests/test_siddon.py
+++ tests/test_siddon.py
@@ -1,6 +1,15 @@
+import nose
 from numpy.testing import *
 import numpy as np
 import siddon
 
 def test_simu():
-    pass
+    import siddon.simu as simu
+    
+    im = simu.fa.infoarrays2infoarray([simu.Image((1, 1)),])
+    obj = simu.Object((1, 1, 1))
+    P = siddon.siddon_lo(im.header, obj.header, obstacle=None)
+    assert_almost_equal(P.todense()[0], 0.39369162)
+
+if __name__ == "__main__":
+    nose.run(argv=['', __file__])
