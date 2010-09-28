b4b9a349efefdbdd8ba8d539a09c78055cab214f
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-28 12:10:14 +0200

    avoid using lo in simple test

--- tests/test_siddon.py
+++ tests/test_siddon.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import nose
 from numpy.testing import *
 import numpy as np
@@ -8,8 +10,14 @@ def test_simu():
     
     im = simu.fa.infoarrays2infoarray([simu.Image((1, 1)),])
     obj = simu.Object((1, 1, 1))
-    P = siddon.siddon_lo(im.header, obj.header, obstacle=None)
-    assert_almost_equal(P.todense()[0], 0.39369162)
+    im[:] = 0.
+    obj[:] = 1.
+    im2 = siddon.projector(im.copy(), obj)
+    assert_almost_equal(im2[0], 0.39369162)
+    im[:] = 1.
+    obj[:] = 0.
+    obj2 = siddon.backprojector(im, obj.copy())
+    assert_almost_equal(obj2[0], 0.39369162)
 
 if __name__ == "__main__":
     nose.run(argv=['', __file__])
