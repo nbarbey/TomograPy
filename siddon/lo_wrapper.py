fe620baeb1c668656225e12dbd70506c4545407c
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-10-07 14:15:01 +0200

    4d model

--- siddon/lo_wrapper.py
+++ siddon/lo_wrapper.py
@@ -25,3 +25,24 @@ def siddon_lo(data_header, cube_header, obstacle=None):
         backprojector(x, y, obstacle=obstacle)
         return y
     return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec, dtype=data.dtype)
+
+def siddon4d_lo(data_header, cube_header, obstacle=None):
+    """
+    A linear operator performing projection and backprojection using
+    Siddon 4-dimensional variation.
+    """
+    data = dataarray_from_header(data_header)
+    data[:] = 0
+    cube = fa.fitsarray_from_header(cube_header)
+    cube[:] = 0
+    def matvec(x):
+        y = dataarray_from_header(data_header)
+        y[:] = 0
+        projector4d(y, x, obstacle=obstacle)
+        return y
+    def rmatvec(x):
+        y = fa.fitsarray_from_header(cube_header)
+        y[:] = 0
+        backprojector4d(x, y, obstacle=obstacle)
+        return y
+    return lo.ndsubclass(cube, data, matvec=matvec, rmatvec=rmatvec, dtype=data.dtype)
