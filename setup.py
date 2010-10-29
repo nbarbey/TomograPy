4475918dd8be906fee1b49178795b2deff386822
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-10-29 15:08:49 +0200

    Cleaner parser call (compiles in python3).

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ from os.path import join
 
 # generate sources from template
 import os
+import imp
 pth = os.getcwd()
-execfile(pth + os.sep + "siddon" + os.sep + "parse_templates.py")
+#execfile(pth + os.sep + "siddon" + os.sep + "parse_templates.py")
+pt_filename = pth + os.sep + "siddon" + os.sep + "parse_templates.py"
+pt = imp.load_source("pt", pt_filename)
+pt.generate_sources()
 
-ext_modules = [Extension(join('siddon', '_' + name + suffix_str % siddon_dict), 
-                         [join('siddon', name + suffix_str + '.c') % siddon_dict,],
+ext_modules = [Extension(join('siddon', '_' + name + pt.suffix_str % siddon_dict),
+                         [join('siddon', name + pt.suffix_str + '.c') % siddon_dict,],
                          include_dirs=[join(get_include(), 'numpy', )],
                          extra_compile_args=['-fopenmp'],
                          extra_link_args=['-fopenmp'],
-                         ) for siddon_dict in siddon_dict_list for name in names]
+                         ) for siddon_dict in pt.siddon_dict_list for name in pt.names]
 
 setup(name='Siddon',
       version='0.2.2',
