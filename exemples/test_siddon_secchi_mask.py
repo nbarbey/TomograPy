Corrected masking in secchi masked exemple.

exemples/test_siddon_secchi_mask.py
@@ -9,11 +9,11 @@ from siddon.solar import read_data
 obsrvtry = ('STEREO_A', 'STEREO_B')
 data = siddon.solar.concatenate(
     [read_data(os.path.join(os.getenv('HOME'), 'data', 'siddon', '171dec08'), 
-               bin_factor=16.,
+               bin_factor=4.,
                obsrvtry=obs,
                time_window=['2008-12-01T00:00:00.000', 
                             '2008-12-15T00:00:00.000'],
-               time_step=16 * 3600.
+               time_step=8 * 3600.
                )
      for obs in obsrvtry])
 data = siddon.solar.sort_data_array(data)
@@ -27,7 +27,7 @@ for i in xrange(data.shape[-1]):
 # make sure it is 64 bits data
 data.header['BITPIX'][:] = -64
 # cube
-shape = np.asarray(3 * (64.,))
+shape = np.asarray(3 * (128.,))
 crpix = shape / 2.
 cdelt = 3. / shape
 header = {'CRPIX1':crpix[0], 'CRPIX2':crpix[1], 'CRPIX3':crpix[2],
@@ -35,24 +35,25 @@ header = {'CRPIX1':crpix[0], 'CRPIX2':crpix[1], 'CRPIX3':crpix[2],
           'CRVAL1':0., 'CRVAL2':0., 'CRVAL3':0.,}
 cube = siddon.fa.zeros(shape, header=header)
 # model
-kwargs = {'obj_rmin':1., 'obj_rmax':1.5, 'data_rmin':0.66, 'data_rmax':1.1,
-          'mask_negative':False}
+kwargs = {'obj_rmin':1., 'obj_rmax':1.5, 'data_rmin':1., 'data_rmax':1.3,
+          'mask_negative':False, 'mask_nan':True}
 P, D, obj_mask, data_mask = siddon.models.srt(data, cube, **kwargs)
+# apply mask to data
+data *= (1 - data_mask)
+data[np.isnan(data)] = 0.
 # hyperparameters
-hypers = cube.ndim * (1e-1, )
+hypers = cube.ndim * (1e0, )
 # inversion
 # expected time
-b = data.flatten()
+b = data.ravel()
 t = time.time()
-bpj = P.T * data.flatten()
+bpj = P.T * b
 print((time.time() - t) * 4 * 100 )
 # real time
 t = time.time()
-#b = data[data_mask == 0]
-b = data.flatten()
-#sol = lo.quadratic_optimization(P, b, D, hypers, maxiter=100)
 sol = lo.acg(P, b, D, hypers, maxiter=100)
 print(time.time() - t)
 # reshape result
 fsol = siddon.fa.asfitsarray(sol.reshape(cube.shape), header=header)
+siddon_path = os.path.join(os.getenv('HOME'), 'data', 'siddon')
 fsol.tofits(os.path.join(siddon_path, "output", "test_siddon_secchi_mask.fits"))