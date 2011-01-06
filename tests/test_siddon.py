1783d4e4db3b242f211b3a2e6dbc1cd9b0b4a9db
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-01-06 06:11:20 +0100

    Test some geometrical behavior of the code.

--- tests/test_siddon.py
+++ tests/test_siddon.py
@@ -47,7 +47,7 @@ def check_full_versus_image(im_h, obj_h):
     if data.dtype == obj.dtype:
         siddon.projector(data, obj)
         for t in xrange(data2.shape[-1]):
-            siddon.image_projector(data2, obj, t)
+            siddon.conic_image_projector(data2, obj, t)
         assert_array_almost_equal(data, data2)
 
 def test_full_versus_image():
@@ -66,40 +66,50 @@ def test_full_unit_vector():
     for im_h in image_headers:
         yield check_full_unit_vector, im_h
 
-# test that the good value is returned from a minimal projection
-# --------------------------------------------------------------
-obj0 = siddon.simu.object_from_header(object_headers64[0])
-obj0[:] = 1.
+# check intersection parameters
+def check_full_intersection_parameters(im_h, obj_h):
+    data = siddon.simu.circular_trajectory_data(**im_h)
+    obj = siddon.fa.fitsarray_from_header(obj_h)
+    if data.dtype == obj.dtype:
+        u = siddon.siddon.C_full_unit_vector(data)
+        a1py, anpy = siddon.siddon.full_intersection_parameters(data, obj, u)
+        a1, an = siddon.siddon.C_full_intersection_parameters(data, obj, u)
 
-data0 = siddon.simu.circular_trajectory_data(**image_headers64[0])
-data0[:] = 1.
+        assert_array_equal(a1, a1py)
+        assert_array_equal(an, anpy)
 
-# class to generate test
-class TestSiddon(object):
-    def __init__(self, func, data0, obj0):
-        self.func = func
-        self.obj0 = obj0
-        self.data0 = data0
-    def __call__(self, data, obj, **kwargs):
-        self.func(data, obj, self.kwargs)
-        assert_almost_equal(obj, obj0)
-        assert_almost_equal(data, data0)
+def test_full_intersection_parameters():
+    for im_h in image_headers:
+        for obj_h in object_headers:
+            yield check_full_intersection_parameters, im_h, obj_h
 
-test_projector_minimal = TestSiddon(siddon.projector, data0, obj0)
+# check that obj.header['PSHAPE'] is returned for ray parallel to axes
 
-def test_simu():
-    import siddon.simu as simu
-    
-    im = simu.fa.infoarrays2infoarray([simu.Image((1, 1)),])
-    obj = simu.Object((1, 1, 1))
-    im[:] = 0.
-    obj[:] = 1.
-    im2 = siddon.projector(im.copy(), obj)
-    assert_almost_equal(im2[0], 0.39369162)
-    im[:] = 1.
-    obj[:] = 0.
-    obj2 = siddon.backprojector(im, obj.copy())
-    assert_almost_equal(obj2[0], 0.39369162)
+def test_parallel_ray():
+    data = siddon.simu.circular_trajectory_data(**image_headers[0])
+    data.header[0]['CRPIX1'] = 1.
+    data.header[0]['CRPIX2'] = 1.
+    for i, lon, lat in ((0, 0, 0), (1, np.pi / 2, 0), (2, np.pi / 2, np.pi / 2)):
+        for obj_h in object_headers64:
+            obj = siddon.fa.fitsarray_from_header(obj_h)
+            obj[:] = 0.
+            if data.dtype == obj.dtype:
+                data[:] = 1.
+                siddon.siddon.backprojector(data, obj)
+                assert_equal(np.sum(obj), obj.header['PSHAPE' + str(i + 1)])
 
-if __name__ == "__main__":
-    nose.run(argv=['', __file__])
+# test that the sum of a bpj in a big cube equal the projection in a
+# cube of 1 pix of the same shape
+
+def test_sum_bpj():
+    data = siddon.simu.circular_trajectory_data(**image_headers[0])
+    data[:] = 1.
+    obj_h1 = object_headers64[-1]
+    obj_h2 = object_headers64[0]
+    obj1 = siddon.fa.fitsarray_from_header(obj_h1)
+    obj2 = siddon.fa.fitsarray_from_header(obj_h2)
+    obj1[:] = 0.
+    obj2[:] = 0.
+    siddon.siddon.backprojector(data, obj1)
+    siddon.siddon.backprojector(data, obj2)
+    assert_equal(np.sum(obj1), obj2)
