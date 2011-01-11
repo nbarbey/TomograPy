d029b925a3eeec5efca85fe4d497989b0ea97055
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-01-11 17:39:58 +0100

    Bug fixes in test_models when applying to stsrt

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,13 +15,14 @@ models = [siddon.models.srt, ]
 
 def check_model(model, im_h, obj_h):
     obj = siddon.simu.object_from_header(obj_h)
+    obj = siddon.fa.InfoArray(data=obj, header=dict(obj.header))
     obj[:] = 1.
     im_h['n_images'] = 100
     im_h['max_lon'] = 3 * np.pi
     data = siddon.simu.circular_trajectory_data(**im_h)
     if obj.dtype == data.dtype:
         P, D, obj_mask, data_mask = model(data, obj, obj_rmin=1)
-        w = (P.T * np.ones(data.size)).reshape(obj.shape)
+        w = (P.T * np.ones(data.size)).reshape(obj_mask.shape)
         is_seen = (w != 0)
         new_obj = fa.FitsArray(obj_mask.shape)
         new_obj = 1.
