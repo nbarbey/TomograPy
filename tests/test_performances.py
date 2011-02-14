aea2d0b46cf2182797aae8323c56404e4e774590
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-02-14 12:58:46 +0100

    Corrections to new functions.

--- tests/test_performances.py
+++ tests/test_performances.py
@@ -49,18 +49,18 @@ def test_image_shape():
     # projection
     pj_times = np.empty(nthread_max + 1)
     pj_times[0] = time.time()
-    for s in image_shapes:
+    for i, s in enumerate(image_shapes):
         data = siddon.centered_stack(siddon.fov(obj, d), s, n_images=64)
         siddon.projector(data, obj)
-        pj_times[nt + 1] = time.time()
+        pj_times[i + 1] = time.time()
     pj_times = pj_times[1:] - pj_times[:-1]
     # backprojection
     bpj_times = np.empty(nthread_max + 1)
     bpj_times[0] = time.time()
-    for s in image_shapes:
+    for i, s in enumerate(image_shapes):
         data = siddon.centered_stack(siddon.fov(obj, d), s, n_images=64)
         siddon.backprojector(data, obj)
-        bpj_times[nt + 1] = time.time()
+        bpj_times[i + 1] = time.time()
     bpj_times = bpj_times[1:] - bpj_times[:-1]
     # pretty print
     text = ''
@@ -79,18 +79,18 @@ def test_map_shape():
     # projection
     pj_times = np.empty(nthread_max + 1)
     pj_times[0] = time.time()
-    for s in cube_shapes:
+    for i, s in enumerate(cube_shapes):
         obj = siddon.centered_cubic_map(3, s)
         siddon.projector(data, obj)
-        pj_times[nt + 1] = time.time()
+        pj_times[i + 1] = time.time()
     pj_times = pj_times[1:] - pj_times[:-1]
     # backprojection
     bpj_times = np.empty(nthread_max + 1)
     bpj_times[0] = time.time()
-    for s in cube_shapes:
+    for i, s in enumerate(cube_shapes):
         obj = siddon.centered_cubic_map(3, s)
         siddon.backprojector(data, obj)
-        bpj_times[nt + 1] = time.time()
+        bpj_times[i + 1] = time.time()
     bpj_times = bpj_times[1:] - bpj_times[:-1]
     # pretty print
     text = ''
@@ -107,4 +107,4 @@ if __name__ == "__main__":
     test_cores()
     test_n_images()
     test_image_shape()
-    test_map_shape
+    test_map_shape()
